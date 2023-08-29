1fbe9570061cadd515fa0b6fbd46fe9fcaec9d68
javen123 <thisjaven@gmail.com>
2023-08-29 09:43:10 -0500

    added policyID and helper script

---
+++ ETRAX/testnet/helper.sh
@@ -0,0 +1,6 @@
+
+# Create Protocol Parameters
+cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.params
+
+# Derive PolicyID from Minting Policy Validator
+cardano-cli transaction policyid --script-file etraxNFT.policy > etraxNFT.pid
